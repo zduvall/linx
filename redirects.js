0db60df8fbd2f9bcbd5cbba4e20593702a1a3608
Zachary <duvallzt@gmail.com>
2024-11-20 23:22:21 -0500

    Update js

--- redirects.js
+++ redirects.js
@@ -1,14 +1,20 @@
 const redirects = {
   g: 'https://www.google.com',
   e: 'https://www.example.com', // example in readme file
+  aridge: {
+    zoom: 'https://zoom.us/j/93256110977?pwd=amFYNzVsMFp5UXo5QzRJRU1HNm14Zz09',
+  },
 };
 
 function handleRedirect() {
-  const key = window.location.pathname.substring(1);
+  const pathSegments = window.location.pathname.substring(1).split('/');
 
-  redirects.hasOwnProperty(key)
-    ? (window.location.href = redirects[key])
-    : (window.location.href = '/');
+  try {
+    redirect = pathSegments.reduce((acc, pS) => acc[pS], redirects);
+    window.location.href = redirect;
+  } catch (error) {
+    window.location.href = '/';
+  }
 }
 
 window.onload = handleRedirect;
